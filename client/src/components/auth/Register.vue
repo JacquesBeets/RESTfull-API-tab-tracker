b50289f011f3ddee75762181fe37c23cb521d633
jacquesbeets <jacques.beets@gmail.com>
2018-05-27 00:18:24 +0200

    Finished Registration Service and added Styling to Registration Page

--- client/src/components/auth/Register.vue
+++ client/src/components/auth/Register.vue
@@ -1,24 +1,42 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <form>
-      <input 
-        type="email" 
-        name="email" 
-        placeholder="email"
-        v-model="email"
-        >
-      <br>
-      <input 
-        type="password" 
-        name="password" 
-        placeholder="password"
-        v-model="password"
-        >
-      <br>
-      <button @click.prevent="register">Register</button>
-    </form>
-    <!-- {{$data}} -->
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col-6">
+        <div class="card shadow">
+          <div class="card-body">
+            <div class="card-title">
+              <h3>Register</h3>
+              <hr>
+            </div>
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address:</label>
+                <input 
+                  v-model="email" 
+                  type="email" 
+                  class="form-control" 
+                  id="exampleInputEmail1" 
+                  aria-describedby="emailHelp" 
+                  placeholder="Enter email">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password:</label>
+                <input 
+                  v-model="password" 
+                  type="password" 
+                  class="form-control" 
+                  id="exampleInputPassword1" 
+                  placeholder="Password">
+              </div>
+              <hr>
+              <button @click.prevent="register" type="submit" class="btn btn-success btn-block">Submit</button>
+            </form>
+              <br v-if="error">
+              <div class="alert alert-danger" role="alert" v-html="error" v-if="error"></div>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -29,18 +47,50 @@ export default {
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
    async register() {
-    const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+     try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+     } catch (error) {
+       this.error = error.response.data.error
+     }
+
     }
+  },
+  computed: {
+    
   }
 }
 </script>
 
+<style scoped>
+
+.container{
+  margin-top: 70px;
+}
+
+h3 {
+  text-align: center;
+  font-family: 'Yellowtail';
+  font-size: 5em;
+}
+
+.card {
+  background-color: rgb(250, 250, 250);
+}
+
+hr {
+  margin: 30px 0;
+}
+
+.error{
+  color: red;
+}
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -2,26 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express();
 
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors()); //Can be a security risk if this enabled. Allows any client around the world can hit the server.
 
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `${req.body.email} was registered succesfully!'`
-  })
-  console.log('Registration Received')
-});
+//All routes defined in routes.js
+require('./routes')(app)
 
 
 
+sequelize.sync()
+.then(() => {
 
+  app.listen(config.port || 8081)
+  console.log(`Server Started on ${config.port}`)
+})
 
-app.listen(process.env.PORT || 8081, function(){
-  console.log('Server Started')
-});
 
